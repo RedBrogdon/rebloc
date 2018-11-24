46352ae68b22d46d430bc3af3dd4b8cff23cc590
Andrew Brogdon <brogdon@gmail.com>
2018-11-23 23:32:00 -0800

    Travis...

--- travis_script.sh
+++ travis_script.sh
@@ -18,14 +18,14 @@ flutter/bin/flutter test
 echo "== Testing rebloc example on Flutter's $FLUTTER_VERSION channel =="
 pushd example
 ../flutter/bin/flutter analyze
-../find lib/. test/. | grep "\.dart$" | xargs ../flutter/bin/flutter format -n
+find lib/. test/. | grep "\.dart$" | xargs ../flutter/bin/flutter format -n
 ../flutter/bin/flutter test
 popd
 
 echo "== Testing rebloc listexample on Flutter's $FLUTTER_VERSION channel =="
 pushd listexample
 ../flutter/bin/flutter analyze
-../find lib/. test/. | grep "\.dart$" | xargs ../flutter/bin/flutter format -n
+find lib/. test/. | grep "\.dart$" | xargs ../flutter/bin/flutter format -n
 ../flutter/bin/flutter test
 popd
 
