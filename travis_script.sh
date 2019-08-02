12f35e9a1ac3a73fdddf7d9b9d2a1dbafd78a73b
Andrew Brogdon <redbrogdon@google.com>
2019-08-02 09:46:20 -0700

    Adding travis stuff

--- travis_script.sh
+++ travis_script.sh
@@ -3,29 +3,14 @@ set -e
 flutter/bin/flutter packages get
 
 echo "== Testing rebloc library on Flutter's $FLUTTER_VERSION channel =="
-pushd lib
-../flutter/bin/flutter analyze
-find . | grep "\.dart$" | xargs ../flutter/bin/flutter format -n
-popd
 
-pushd test
-../flutter/bin/flutter analyze
+flutter/bin/flutter analyze
 find . | grep "\.dart$" | xargs ../flutter/bin/flutter format -n
-popd
-
 flutter/bin/flutter test
 
 echo "== Testing rebloc example on Flutter's $FLUTTER_VERSION channel =="
 pushd example
-../flutter/bin/flutter analyze
-find lib/. test/. | grep "\.dart$" | xargs ../flutter/bin/flutter format -n
-../flutter/bin/flutter test
-popd
-
-echo "== Testing rebloc listexample on Flutter's $FLUTTER_VERSION channel =="
-pushd listexample
-../flutter/bin/flutter analyze
-find lib/. test/. | grep "\.dart$" | xargs ../flutter/bin/flutter format -n
+../flutter/bin/flutter packages get
 ../flutter/bin/flutter test
 popd
 
