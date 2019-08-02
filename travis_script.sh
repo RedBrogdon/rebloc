19a014da0a544a03a3ea647d01daf6b37c6354d7
Andrew Brogdon <redbrogdon@google.com>
2019-08-02 10:25:03 -0700

    Travis, part 2.

--- travis_script.sh
+++ travis_script.sh
@@ -3,14 +3,22 @@ set -e
 flutter/bin/flutter packages get
 
 echo "== Testing rebloc library on Flutter's $FLUTTER_VERSION channel =="
-
-flutter/bin/flutter analyze
+pushd lib
+../flutter/bin/flutter analyze
 find . | grep "\.dart$" | xargs ../flutter/bin/flutter format -n
+popd
+
+pushd test
+../flutter/bin/flutter analyze
+find . | grep "\.dart$" | xargs ../flutter/bin/flutter format -n
+popd
+
 flutter/bin/flutter test
 
 echo "== Testing rebloc example on Flutter's $FLUTTER_VERSION channel =="
 pushd example
-../flutter/bin/flutter packages get
+../flutter/bin/flutter analyze
+find lib/. test/. | grep "\.dart$" | xargs ../flutter/bin/flutter format -n
 ../flutter/bin/flutter test
 popd
 
